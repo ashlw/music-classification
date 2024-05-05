4b9b27999c3fe5ca1d8bb17e16e38ba9e82d0013
ashlw <alwang@usc.edu>
2024-05-05 14:21:22 -0700

    updated feature extraction and file parsing

--- baseline.py
+++ baseline.py
@@ -8,26 +8,29 @@ import argparse
 
 DEV = False
 
-# refactor (perhaps parse files in a diff file to speed things up)
-# returns an array of midi data given composer
-def parseFiles(composer):
-    path = "data/" + composer + '/'
-    paths = os.listdir(path)
-    midi_data = []
-    for p in paths:
-        file = path + p
-        try:
-            midi_data.append(pretty_midi.PrettyMIDI(file))
-        except:
-            print("Error with " + file)
-    return midi_data
+def readData():
+    data = open('data_split.txt', 'r')
+    train_data = eval(data.readline())
+    dev_data = eval(data.readline())
+    test_data = eval(data.readline())
+    return train_data, dev_data, test_data
 
-# take the mean of pitch frequencies that is weighted by note duration
+# wrap files into pretty midi and get composer labels
+def parseFiles(data):
+    x, y = [], []
+    for d in data:
+        path = os.path.split(d)
+        y.append(path[0])
+        path = "data/" + d
+        x.append(pretty_midi.PrettyMIDI(path))
+
+    return x, np.array(y)
+
 def extract(midi_list):
     avg_pitches = []
     for midi in midi_list:
         hist = midi.get_pitch_class_histogram(use_duration=True)
-        avg_pitches.append(np.mean(hist))
+        avg_pitches.append(hist)
     return avg_pitches
 
 # given a list of composers, transform into 0s and 1s for binary classification
@@ -51,26 +54,21 @@ def main():
         print("IN DEV MODE")
         DEV = True
 
+    train_data, dev_data, test_data = readData()
+    x_train, y_train = parseFiles(train_data)
+    x_dev, y_dev = parseFiles(dev_data)
+    x_test, y_test = parseFiles(test_data)
+    print('Finished parsing')
+
+    x_train = np.array(extract(x_train))
+    x_dev = np.array(extract(x_dev))
+    x_test = np.array(extract(x_test))
+
     composers = os.listdir("data")
-    x_data, y = [],[]
 
-    for c in composers:
-        midi_data = parseFiles(c)
-        length = len(midi_data)
-        y.extend([c] * length)
-        x_data.extend(extract(midi_data))
-    y_data = np.array(y)
-    x_data = np.array(x_data)
-    x_data = x_data.reshape(-1, 1)
-
-    # split 90/10 for train+dev/test
-    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.1, shuffle=True)
-    # split again for the 70/20 train/dev
-    x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.22, shuffle=True)
-    if DEV == True:
+    if DEV:
         x_test = x_dev
         y_test = y_dev
-
     for c in composers:
         y_train_c = classify(c, y_train)
         y_test_c = classify(c, y_test)
