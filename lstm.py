ec18f25ac1ae73b8076c133ce59de874648c652d
ashlw <alwang@usc.edu>
2024-10-02 09:32:14 -0700

    it is working.

--- lstm.py
+++ lstm.py
@@ -1,12 +1,11 @@
 import pretty_midi
 import numpy as np
-import pandas as pd
 import tensorflow as tf
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-#ref: # https://github.com/ruohoruotsi/LSTM-Music-Genre-Classification/blob/master/lstm_genre_classifier_pytorch.py
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+import os
+# https://github.com/ruohoruotsi/LSTM-Music-Genre-Classification/blob/master/lstm_genre_classifier_pytorch.py
 
 def readData():
     data = open('data_split.txt', 'r')
@@ -20,61 +19,60 @@ def parseFiles(data):
     x, y = [], []
     for d in data:
         path = os.path.split(d)
-        y.append(path[0])
+        # HARDCODING IS BAD I KNOW SORY
+        if path[0] == 'bach':
+            y.append(0)
+        elif path[0] == 'beethoven':
+            y.append(1)
+        elif path[0] == 'chopin':
+            y.append(2)
+        else:
+            y.append(3)
         path = "data/" + d
         x.append(pretty_midi.PrettyMIDI(path))
 
     return x, np.array(y)
 
-# from https://www.tensorflow.org/tutorials/audio/music_generation#create_the_training_dataset
-def midi_to_notes(midi_file: str) -> pd.DataFrame:
-  pm = pretty_midi.PrettyMIDI(midi_file)
-  instrument = pm.instruments[0]
-  notes = collections.defaultdict(list)
+# https://www.tensorflow.org/tutorials/audio/music_generation#create_the_training_dataset
+def midi_to_notes(pm: pretty_midi):
+  # pm = pretty_midi.PrettyMIDI(midi_file)
+  notes = []
+  for instrument in pm.instruments:
+      for note in instrument.notes:
+          # Each note has a start time, end time, and pitch
+          notes.append([note.start, note.end, note.pitch, note.velocity])
+  # Sort notes by start time to ensure chronological order
+  notes.sort(key=lambda x: x[0])
+  return notes
 
-  # Sort the notes by start time
-  sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
-  prev_start = sorted_notes[0].start
+def create_seq(notes: list, composer: int, seq_len: int):
+    # modify this part for more features
+    input_sequences = []
+    label = []
+    pitches = [note[2] for note in notes]
+    for i in range(0, len(pitches) - seq_len):
+        input_sequences.append(pitches[i:i + seq_len])
+        label.append(composer)
 
-  for note in sorted_notes:
-    start = note.start
-    end = note.end
-    notes['pitch'].append(note.pitch)
-    notes['start'].append(start)
-    notes['end'].append(end)
-    notes['step'].append(start - prev_start)
-    notes['duration'].append(end - start)
-    prev_start = start
+    return input_sequences, label
 
-  return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+# create seq and labels
+def label(data: list, y: list, seq_length: int):
+    # for each midi and corresponding composer label
+    total_seq = []
+    labels = []
+    for i in range(len(data)):
+        # extract features
+        pm = midi_to_notes(data[i])
+        seq, label = create_seq(pm, y[i], seq_length)
+        total_seq.extend(seq)
+        labels.extend(label)
+        # num_sequences, sequence_length
+    total_seq = np.array(total_seq)
+    total_seq = np.reshape(total_seq, (total_seq.shape[0], total_seq.shape[1], 1))
+    labels = np.array(labels)
+    return total_seq, labels
 
-class LSTM(nn.Module):
-    # input dim -> number of features
-    # output dim -> number of classes
-    # hidden dim -> number of nodes in unit, equiv to CNN neurons
-    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=4, num_layers=2):
-        super(LSTM, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.batch_size = batch_size
-        self.num_layers = num_layers
-        # setup LSTM layer
-        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
-        # setup output layer
-        self.linear = nn.Linear(self.hidden_dim, output_dim)
-
-    def forward(self, input, hidden=None):
-        lstm_out, hidden = self.lstm(input, hidden)
-        logits = self.linear(lstm_out[-1])
-        scores = F.log_softmax(logits, dim=1)
-        return scores, hidden
-
-    def get_accuracy(self, logits, target):
-        corrects = (
-                torch.max(logits, 1)[1].view(target.size()).data == target.data
-        ).sum()
-        accuracy = 100.0 * corrects / self.batch_size
-        return accuracy.item()
 
 def main():
     train_data, dev_data, test_data = readData()
@@ -82,22 +80,33 @@ def main():
     x_dev, y_dev = parseFiles(dev_data)
     # x_test, y_test = parseFiles(test_data)
 
-    # TODO: extract features
-
-    # convert to tensors
-    x_train = torch.from_numpy(x_train).type(torch.Tensor)
-    x_dev = torch.from_numpy(x_dev).type(torch.Tensor)
-    # x_test = torch.from_numpy(x_test).type(torch.Tensor)
-
-    # Targets is a long tensor of size (N,) which tells the true class of the sample.
-    y_train = torch.from_numpy(y_train).type(torch.LongTensor)
-    y_dev = torch.from_numpy(y_dev).type(torch.LongTensor)
-    # y_test = torch.from_numpy(y_test).type(torch.LongTensor)
-
-    batch_size = 35  # num of training examples per minibatch
+    # HYPERPARAMS
+    seq_length = 50
+    batchsize = 70  # num of training examples per minibatch
     num_epochs = 100
+    lr = 0.001
 
-    # initialize model
+    # split into sequences and corresponding composers
+    x_train, y_train = label(x_train, y_train, seq_length)
+    x_dev, y_dev = label(x_dev, y_dev, seq_length)
+
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(seq_length, 1), return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(128))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(4, activation='softmax'))  # num_composers = 4
+
+    opt = Adam(learning_rate=lr)
+    # categorical_crossentropy requires encoded labels, i can change this in the future
+    model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=num_epochs, batch_size=batchsize, validation_data=(x_dev, y_dev))
+
+    # Evaluate
+    test_loss, test_accuracy = model.evaluate(x_dev, y_dev)
+    print(f"Test Accuracy: {test_accuracy * 100:.2f}%")
+
 
 if __name__ == '__main__':
     main()
